6acffd7c879b3a103823aa33c6cfc323a05ca54b
Vkscoma <vkscoma@gmail.com>
2023-03-08 06:49:04 -0800

    Finishing Touches

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const grid = document.querySelector('.grid-container');
 const button = document.querySelector('#reset-btn');
+const blackButton = document.querySelector('#black-btn');
+const randomButton = document.querySelector('#random-btn');
 
 function createGridItems() {
     const gridItems = document.createElement('div');
@@ -15,11 +17,31 @@ function createGrid() {
 
 createGrid();
 
+function clearGrid() {
+    grid.innerHTML = '';
+}
+
 grid.addEventListener('mouseover', (e) => {
     e.target.style.backgroundColor = 'black';
 });
 
 button.addEventListener('click', () => {
-    grid.innerHTML = '';
+    clearGrid();
     createGrid();
+});
+
+blackButton.addEventListener('click', () => {
+    clearGrid();
+    createGrid();
+    grid.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'black';
+    });
+});
+
+randomButton.addEventListener('click', () => {
+    clearGrid();
+    createGrid();
+    grid.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    });
 });
